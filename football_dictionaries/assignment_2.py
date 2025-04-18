f52190641378d7b07cadde7b307116cbd2fa884a
Maha-am <135711723+Maha-am@users.noreply.github.com>
2025-04-18 15:31:08 +0100

    Update assignment_2.py

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,11 @@
+from itertools import groupby
 def players_by_position(squads_list):
-    pass
+    keys = ['number', 'position', 'name', 'date_of_birth', 'caps', 'club', 'country', 'club_country', 'year']
+    players = [dict(zip(keys, player)) for player in squads_list]
+    sorted_player = sorted(players, key=lambda p: p['position'])
+    grouped = {}
+    for position, group in groupby(sorted_player, key=lambda p: p['position']):
+        grouped[position] = list(group)
+    return grouped
+
+
