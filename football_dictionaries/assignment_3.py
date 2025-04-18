072873289f173992d5345656d819b89586c75238
Maha-am <135711723+Maha-am@users.noreply.github.com>
2025-04-18 16:25:40 +0100

    Update assignment_3.py

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,21 @@
 def players_by_country_and_position(squads_list):
-    pass
+    keys = ['number', 'position', 'name', 'date_of_birth', 'caps', 'club', 'country', 'club_country', 'year']
+    players = [dict(zip(keys, player)) for player in squads_list]
+
+    grouped = {}
+
+    for player in players:
+        country = player['country']
+        position = player['position']
+
+        if country not in grouped:
+            grouped[country] = {}
+
+        position_dict = grouped[country]
+
+        if position not in position_dict:
+            position_dict[position] = []
+
+        position_dict[position].append(player)
+
+    return grouped
